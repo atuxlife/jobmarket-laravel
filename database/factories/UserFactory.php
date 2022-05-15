e1dfdd6f7832f9411e74ca85ac4fc010ec5f7f8a
Carlos Jacquin Venencia <atuxlife@gmail.com>
2022-05-14 22:16:03 -0500

    Refactor de UserFactory para generar el hash de la clave de mejor manera

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,11 +18,18 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+
+        $hash = Hash::make('password', [
+            'memory'    => 1024,
+            'time'      => 2,
+            'threads'   => 2,
+        ]);
+
         return [
             'name'              => $this->faker->name(),
             'email'             => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password'          => $hash, // password
             'remember_token'    => Str::random(10),
             'role'              => $this->faker->randomElement(['A', 'U']),
             'document_type'     => $this->faker->randomElement(['CC', 'CE', 'PS']),
